ca5d195707ffc387d0e38a753f0b8f68730cc35b
Aditya Kumar <adityakumar2001@outlook.com>
2026-06-29 12:08:58 -0400

    lab 0

--- lab00/lab00/lab00.py
+++ lab00/lab00/lab00.py
@@ -5,5 +5,7 @@ def twenty_twenty_four():
     >>> twenty_twenty_four()
     2024
     """
-    return ______
+    return 2*7 + 3*(7*6*5*4*3*2*1 - 5*(6*5*4*3*2*1 + 5*4*3*2*1 + 4*3*2*1 + 3*2*1 + 2*1 + 1 + 1))
+
+print(twenty_twenty_four())
 
