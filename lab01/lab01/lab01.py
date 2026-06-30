a318e69a6de80fd85848b8ef92c5f1545dd0dbb5
Aditya Kumar <adityakumar2001@outlook.com>
2026-06-30 03:45:10 -0400

    Lab 1 done

--- lab01/lab01/lab01.py
+++ lab01/lab01/lab01.py
@@ -8,7 +8,7 @@ def digit(n, k):
     >>> digit(3579, 10)
     0
     """
-    return ____
+    return (n//10**k)%10
 
 
 def middle(a, b, c):
@@ -26,7 +26,7 @@ def middle(a, b, c):
     >>> middle(30, 5, 40)
     30
     """
-    return ____
+    return a + b + c - max(a,b,c) - min(a,b,c)
 
 
 def falling(n, k):
@@ -42,6 +42,10 @@ def falling(n, k):
     1
     """
     "*** YOUR CODE HERE ***"
+    if k == 0:
+        return 1
+    while k != 0:
+        return n*falling(n-1, k-1)
 
 
 def divisible_by_k(n, k):
@@ -65,6 +69,12 @@ def divisible_by_k(n, k):
     0
     """
     "*** YOUR CODE HERE ***"
+    count = 0
+    for i in range(1, n+1):
+        if i % k == 0:
+            print(i)
+            count +=1
+    return count
 
 
 def sum_digits(y):
@@ -81,6 +91,9 @@ def sum_digits(y):
     6
     """
     "*** YOUR CODE HERE ***"
+    if y == 0:
+        return 0
+    return y%10 + sum_digits(y//10)
 
 
 def double_eights(n):
@@ -99,4 +112,15 @@ def double_eights(n):
     False
     """
     "*** YOUR CODE HERE ***"
+    single = False
+    while n > 0:
+        if single and n % 10 == 8:
+            return True
+        elif not single and n % 10 == 8:
+            single = True
+        else:
+            single = False
+        n = n//10
+    return False
+        
 
